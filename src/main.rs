6196a8d80719e6be47db98f078df3df124474b57
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2021-01-11 01:33:58 +0500

    Implement serialization for RESP Array and RESP BulkString types. Refactor main with respect of changes

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ use async_std::prelude::*;
 async fn main() -> io::Result<()> {
     // creating TCP connection to redis server
     let mut stream = TcpStream::connect("localhost:6379").await?;
-    // RESP encoded array: PING command
-    let command = b"*1\r\n$4\r\nPING\r\n";
+    let mut buffer = vec![];
+    let command = RespValue::Array(vec![RespValue::BulkString(b"PING".to_vec())]);
+    command.serialize(&mut buffer);
     // sending command to redis server
-    stream.write_all(command).await?;
+    stream.write_all(&buffer).await?;
     let mut buffer = vec![0; 1024];
     // read from TCP connection. Expect PONG string
     let bytes_read = stream.read(&mut buffer).await?;
@@ -33,4 +34,36 @@ fn parse_response(buffer: &[u8]) -> Result<&str, String> {
     Ok(std::str::from_utf8(&buffer[1..buffer.len() - 2]).unwrap())
 }
 
+enum RespValue {
+    Array(Vec<RespValue>),
+    BulkString(Vec<u8>),
+    SimpleString(String),
+    Error(Vec<u8>),
+    Integers(i64),
+}
 
+impl RespValue {
+    fn serialize(self, buf: &mut Vec<u8>) {
+        match self {
+            RespValue::Array(values) => {
+                buf.push('*' as u8);
+                buf.append(&mut format!("{}", values.len()).into_bytes());
+                buf.push('\r' as u8);
+                buf.push('\n' as u8);
+                for value in values {
+                    value.serialize(buf)
+                }
+            }
+            RespValue::BulkString(mut data) => {
+                buf.push('$' as u8);
+                buf.append(&mut format!("{}", data.len()).into_bytes());
+                buf.push('\r' as u8);
+                buf.push('\n' as u8);
+                buf.append(&mut data);
+                buf.push('\r' as u8);
+                buf.push('\n' as u8);
+            }
+            _ => unimplemented!(),
+        }
+    }
+}
