67b3568c8db83d97e63bef0de5810a1dc3b84760
Alisher Matkurbanov <alishermatkurbanov0408@gmail.com>
2021-01-11 03:00:35 +0500

    Add structs and impls for Client and Error. Refactored code. Add set and get functions

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,82 @@
-use async_std::net::TcpStream;
+use async_std::net::{ToSocketAddrs, TcpStream};
 use async_std::io;
 use async_std::prelude::*;
 
 
 #[async_std::main]
 async fn main() -> io::Result<()> {
-    // creating TCP connection to redis server
-    let mut stream = TcpStream::connect("localhost:6379").await?;
-    let mut buffer = vec![];
-    let command = RespValue::Array(vec![RespValue::BulkString(b"PING".to_vec())]);
-    command.serialize(&mut buffer);
-    // sending command to redis server
-    stream.write_all(&buffer).await?;
-    let mut buffer = vec![0; 1024];
-    // read from TCP connection. Expect PONG string
-    let bytes_read = stream.read(&mut buffer).await?;
-    // println!("{:?}", std::str::from_utf8(&buffer[..bytes_read]).unwrap());
-    println!("{:?}", parse_response(&buffer[0..bytes_read]));
+    let mut client = Client::new("localhost:6379").await?;
+    client.set("Alisher".into(), "New".into()).await.unwrap();
+    println!("{:?}", client.get("Alisher".into()).await.unwrap());
     Ok(())
 }
 
-fn parse_response(buffer: &[u8]) -> Result<&str, String> {
+struct Client {
+    stream: TcpStream,
+}
+
+
+impl Client {
+    async fn new<A: ToSocketAddrs>(addr: A) -> Result<Client, io::Error> {
+        let stream = TcpStream::connect(addr).await?;
+        Ok(Client { stream })
+    }
+}
+
+impl Client {
+    async fn set(&mut self, key: String, value: String) -> Result<(), Error> {
+        let command = RespValue::Array(vec![
+            RespValue::BulkString(b"SET".to_vec()),
+            RespValue::BulkString(key.into_bytes()),
+            RespValue::BulkString(value.into_bytes()),
+        ]);
+        let mut buffer = vec![];
+        command.serialize(&mut buffer);
+        self.stream.write_all(&buffer).await?;
+        let bytes_read = self.stream.read(&mut buffer).await?;
+        parse_response(&buffer[..bytes_read])?;
+        Ok(())
+    }
+
+    async fn get(&mut self, key: String) -> Result<String, Error> {
+        let command = RespValue::Array(vec![
+            RespValue::BulkString(b"GET".to_vec()),
+            RespValue::BulkString(key.into_bytes()),
+        ]);
+        let mut buffer = vec![];
+        command.serialize(&mut buffer);
+        self.stream.write_all(&buffer).await?;
+        let bytes_read = self.stream.read(&mut buffer).await?;
+        let resp = parse_response(&buffer[..bytes_read])?;
+        Ok(resp.to_owned())
+    }
+}
+
+
+#[derive(Debug)]
+struct Error {}
+
+impl std::convert::From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error {}
+    }
+}
+
+
+fn parse_response(buffer: &[u8]) -> Result<&str, Error> {
     if buffer.is_empty() {
-        return Err("Empty buffer".into());
+        return Err(Error {});
+        // return Err("Empty buffer".into());
     }
     // parsing response according to RESP syntax
     if buffer[0] == ('-' as u8) {
-        return Err(format!(
-            "Error Response: {:?}",
-            &buffer[1..buffer.len() - 2]));
+        return Err(Error {});
     }
-
+    //         (
+    //         "Error Response: {:?}",
+    //         &buffer[1..buffer.len() - 2]));
+    // }
+    // TODO: Remove characters in string LEN\r\nDATA
     Ok(std::str::from_utf8(&buffer[1..buffer.len() - 2]).unwrap())
 }
 
